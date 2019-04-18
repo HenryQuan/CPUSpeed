47a842bb18dd58e124815aa48c95229f385668e6
Yiheng (NUC) <development.henryquan@gmail.com>
2019-04-18 19:35:08 +1000

    run script inside app

--- app/src/main/java/com/yihengquan/cpuspeed/MainActivity.java
+++ app/src/main/java/com/yihengquan/cpuspeed/MainActivity.java
@@ -2,6 +2,12 @@ package com.yihengquan.cpuspeed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Try to get root
+        try {
+            String[] commands = {"su -c 'dumpstate > /sdcard/log1.txt'"};
+            Process p = Runtime.getRuntime().exec("/system/bin/sh -");
+            DataOutputStream os = new DataOutputStream(p.getOutputStream());
+            for (String tmpCmd : commands) {
+                os.writeBytes(tmpCmd+"\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
