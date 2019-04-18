18cf83467779717923e68c37710f676803b9ec8b
Yiheng (NUC) <development.henryquan@gmail.com>
2019-04-18 20:40:20 +1000

    input validation

--- app/src/main/java/com/yihengquan/cpuspeed/MainActivity.java
+++ app/src/main/java/com/yihengquan/cpuspeed/MainActivity.java
@@ -3,6 +3,7 @@ package com.yihengquan.cpuspeed;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -24,25 +25,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setSpeed(View view) {
-        String input = ((EditText)findViewById(R.id.speedText)).getText().toString();
-        int core = Runtime.getRuntime().availableProcessors();
-        Toast.makeText(this, "Hello World\n" + input + core, Toast.LENGTH_SHORT).show();
+        EditText inputBox = findViewById(R.id.speedText);
+        String input = inputBox.getText().toString();
+        // Hide virtual keyboard
+        InputMethodManager imm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(inputBox.getWindowToken(), 0);
+
+        try {
+            // If input is a number
+            int speed = Integer.parseInt(input);
+            int core = Runtime.getRuntime().availableProcessors();
+            setCPUSpeed(speed, core);
+        } catch (NumberFormatException e) {
+            // Not valid input
+            Toast.makeText(this, "Input is not valid", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    private void setCPUSpeed(int core) {
+    private void setCPUSpeed(int speed, int core) {
         // Get a list for commands
-        ArrayList<String> commands;
+        ArrayList<String> commands = new ArrayList<>();
         for (int i = 0; i < core; i++) {
-            commands.add()
+            String curr = String.format("su -c 'echo %d > /sys/devices/system/cpu/cpu%d/cpufreq/scaling_max_freq'", speed, i);
+            commands.add(curr);
         }
 
-        // Try to get root
+        // Try to get root and run the script
         try {
-
+            String[] c = (String[])commands.toArray();
             Process p = Runtime.getRuntime().exec("/system/bin/sh -");
             DataOutputStream os = new DataOutputStream(p.getOutputStream());
-            for (String tmpCmd : commands) {
-                os.writeBytes(tmpCmd + "\n");
+            for (String cmd : c) {
+                os.writeBytes(cmd + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
