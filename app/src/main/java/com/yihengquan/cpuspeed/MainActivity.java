2484dfb63231ce4b4a6c199c5ca071dc208818e5
Yiheng (NUC) <development.henryquan@gmail.com>
2019-06-21 21:41:32 +1000

    upgrade gradle, version number and whats new message

--- app/src/main/java/com/yihengquan/cpuspeed/MainActivity.java
+++ app/src/main/java/com/yihengquan/cpuspeed/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private int currMaxFreq = 0;
     private int currMinFreq = 0;
 
-    private final String appVersion = "1.0.2";
+    private final String appVersion = "1.0.3";
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
@@ -199,7 +199,7 @@ public class MainActivity extends AppCompatActivity {
         if (whatsnew) {
             new AlertDialog.Builder(this)
                 .setTitle(String.format("Version %s", appVersion))
-                .setMessage("- Improve sliding experience\n- Support more devices")
+                .setMessage("- Added more CPU info\n- Added about, share, support buttons\n- Bug fixed\n\nThank you for 1000 downloads ^_^")
 
                 // Specifying a listener allows you to take an action before dismissing the dialog.
                 // The dialog is automatically dismissed when a dialog button is clicked.
