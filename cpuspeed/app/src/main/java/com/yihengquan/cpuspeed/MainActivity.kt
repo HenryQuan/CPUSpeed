f44aa7aea423ea5e09ac78ba662cc0ecac13d94d
Yiheng <development.henryquan@gmail.com>
2021-04-04 11:49:15 +1000

    added flutter module and linked everything
    The app size is huge now, lol. Finally, it is working on my computer. I think it might be because of the HDD so it doesn't work. This is the only reason I could think of.

--- cpuspeed/app/src/main/java/com/yihengquan/cpuspeed/MainActivity.kt
+++ cpuspeed/app/src/main/java/com/yihengquan/cpuspeed/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.yihengquan.cpuspeed
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FlutterActivity() {
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
